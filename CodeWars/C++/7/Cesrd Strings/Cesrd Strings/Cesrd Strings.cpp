7ed6e4531bc7cdcb2e4051e6eb92e26ede144aa1
Viilture <53421674+Viilture@users.noreply.github.com>
2022-06-26 21:13:48 +0300

    +Ce*s*r*d Strings

---
+++ CodeWars/C++/7/Cesrd Strings/Cesrd Strings/Cesrd Strings.cpp
@@ -0,0 +1,56 @@
+﻿// Cesrd Strings.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+std::string uncensor(std::string infected,
+    std::string discovered) 
+{
+    for (auto &i : infected)
+    {
+        if (i == '*')
+        {
+            i = discovered.at(0);
+            discovered.erase(0,1);
+        }           
+    }
+    return infected;
+}
+
+std::string uncensor2(std::string infected,
+    std::string discovered)
+{
+    std::string out;
+    
+    std::transform(infected.cbegin(), infected.cend(),
+        )
+
+}
+
+
+int main()
+{
+    std::cout << uncensor("*h*s *s v*ry *tr*ng*", "Tiiesae") << std::endl;
+
+    std::cout << uncensor("A**Z*N*", "MAIG") << std::endl;
+
+    std::cout << uncensor("xyz", "") << std::endl;
+
+    std::cout << uncensor("", "") << std::endl;
+
+    std::cout << uncensor("***", "abc") << std::endl;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ CodeWars/C++/7/Looking for a benefactor/Looking for a benefactor/Looking for a benefactor.cpp
@@ -0,0 +1,42 @@
+﻿// Looking for a benefactor.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <stdexcept>
+
+class NewAverage
+{
+public:
+    static long long newAvg(std::vector<double>& arr, double navg)
+    {
+        long double mid = std::accumulate(arr.begin(),arr.end(),0)/arr.size();
+        std::cout << mid << " " << navg << std::endl;
+        if (mid > navg)
+        {
+        
+        }
+        else
+        {
+            return navg * (arr.size() + 1) - std::accumulate(arr.begin(), arr.end(), 0);
+        }
+
+    }
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    std::vector<double> arr1 = { 14.0, 30.0, 5.0, 7.0, 9.0, 11.0, 16.0 };
+    std::cout << NewAverage::newAvg(arr1, 90) << std::endl;
+
+    std::vector<double> arr2 = { 14, 30, 5, 7, 9, 11, 15 };
+    std::cout << NewAverage::newAvg(arr2, 92) << std::endl;
+
+    std::vector<double> arr3 = { 14, 30, 5, 7, 9, 11, 15 };
+    std::cout << NewAverage::newAvg(arr3, 2) << std::endl;
+}
